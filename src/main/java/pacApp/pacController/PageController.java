0eb75a51244678e91f23c4ad3c31fa986b394fc9
eeren <eeren@DESKTOP-QCVPO15>
2020-11-30 23:45:58 +0100

    change page controller to esp32

--- src/main/java/pacApp/pacController/PageController.java
+++ src/main/java/pacApp/pacController/PageController.java
@@ -2,9 +2,12 @@ package pacApp.pacController;
 
 import java.net.Socket;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pacApp.pacSocket.SocketServer;
 import pacModel.Temp;
@@ -29,9 +32,35 @@ public class PageController {
     		model.addAttribute("test", t.toString());
     	return "index";
     }
+    Temp tempEsp32;
+    @GetMapping("/tempscan")
+    public ResponseEntity<String> tempscan(@RequestParam String temperature, Model model) {
+    	System.out.println(temperature);
+    	Temp t = new Temp(temperature);
+    	tempEsp32 = t;
+    	return new ResponseEntity<>("index", HttpStatus.OK);
+    }    
+    @GetMapping("/index")
+    public String indexEsp32(Model model) {
+    	if (tempEsp32 != null) {
+	    	if (tempEsp32.getValue() > 39.00) {
+	    		model.addAttribute("message", "Ihr Körpertemperatur ist zu HOCH!");        	
+	    	}  else if(tempEsp32.getValue() > 34.00)  {
+	    		model.addAttribute("message", "Ihr Körpertemperatur ist normal!");  
+	    	} else {
+	    		model.addAttribute("message", "Ihr Körpertemperatur ist zu niedrig!");  
+	    	}
+	    	if (tempEsp32.getValue() < 20) 
+	    		model.addAttribute("test", " - ");
+	    	else
+	    		model.addAttribute("test", tempEsp32.toString());
+    	}
+    	return "index";
+    }
+    
     @GetMapping("/test")
     public String indextest(Model model) {
-    	model.addAttribute("message", "hallo");
+    	model.addAttribute("test", "Hallo Welt");
         return "index";
     }
    
